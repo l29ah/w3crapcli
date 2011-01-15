6884b10fe95f22520ed7254c7bbe6509d0b5ee7f
Web Uploader <server@w3crapcli>
2011-01-15 17:33:31 +0300

    removed older versions of lastfm.pl

--- last.fm/lastfm.pl
+++ last.fm/lastfm.pl
@@ -1,7 +1,10 @@
 #!/usr/bin/perl
+
+# ~/.config/lastfm looks like this:
+# login = 'vpupkin';
+# password = 'mycoolpassword';
+
 $hs_url = "http://post.audioscrobbler.com/";
-$login = "L29Ah";
-$passwd	= "lol";
 $client_id = "lsd";
 $client_version = "1.0.4";
 $debug = 1;
@@ -9,9 +12,16 @@ $debug = 1;
 
 use LWP::UserAgent;
 use Digest::MD5 qw(md5_hex);
+use URI::Escape;
 
 die "Usage: lastfm.pl <artist> <title> <album> <length>" unless $#ARGV==3;
 $ua = LWP::UserAgent->new;
+
+do "$ENV{HOME}/.config/lastfm";
+
+foreach ("login", "password") {
+	die "No $_ in ~/.config/lastfm" unless ${$_};
+}
 
 my $resp = $ua->get($hs_url."?hs=true&p=1.1&c=$client_id&v=$client_version&u=$login");
 die "Unable to handshake with $hs_url" unless $resp->is_success;
@@ -19,9 +29,9 @@ die "Unable to handshake with $hs_url" unless $resp->is_success;
 print "Status: $status\nHash: $hash\nUrl: $url\n$int" if $debug;
 die "Handshake failed, server returned: $status=\n" unless $status =~ /UP(TO)?DATE/;
 
-$hash = md5_hex(md5_hex($passwd).$token);
+$hash = md5_hex(md5_hex($password).$token);
 @t = gmtime();
-$req_str = "u=$login&s=$hash&a[0]=".StrEscaped($ARGV[0]).'&t[0]='.StrEscaped($ARGV[1]).'&b[0]='.StrEscaped($ARGV[2]).'&m[0]=&l[0]='.StrEscaped($ARGV[3]).'&i[0]='.StrEscaped(sprintf('%04d-%02d-%02d %02d:%02d:%02d', $t[5] + 1900, $t[4] + 1, @t[3, 2, 1, 0]));
+$req_str = "u=$login&s=$hash&a[0]=".uri_escape($ARGV[0]).'&t[0]='.uri_escape($ARGV[1]).'&b[0]='.uri_escape($ARGV[2]).'&m[0]=&l[0]='.uri_escape($ARGV[3]).'&i[0]='.uri_escape(sprintf('%04d-%02d-%02d %02d:%02d:%02d', $t[5] + 1900, $t[4] + 1, @t[3, 2, 1, 0]));
 
 print "$req_str\n" if $debug;
 
@@ -34,8 +44,3 @@ $resp = $ua->request($req);
 print "Server returned: $status\n" if $debug;
 die "Scrobble failed, server returned: $status\n" unless $status == "OK";
 
-sub StrEscaped {
-  my ($str)=@_;
-  $str=~s/([^0-9A-Za-z\\?&=:;])/sprintf("%%%x", ord($1))/eg;
-  return $str
-}
