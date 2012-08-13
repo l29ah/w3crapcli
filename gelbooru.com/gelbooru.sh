a70d92d1c7c4c82175d69898170f19cb8daccd89
Sergey Alirzaev <zl29ah@gmail.com>
2012-08-13 11:34:26 +0400

    * gelbooru: parallelized

--- gelbooru.com/gelbooru.sh
+++ gelbooru.com/gelbooru.sh
@@ -2,7 +2,7 @@
 # Gelbooru parser
 # Download pictures from http://gelbooru.com by tags
 # Usage: ./gelbooru.sh [tags] [folder for pictures (will create if unset)]
-# Depends: bash, wget, xmlstarlet
+# Depends: bash, wget, xmlstarlet, GNU parallel
 # License: Public Domain, http://creativecommons.org/publicdomain/mark/1.0/
 
 # Check for parameters
@@ -30,9 +30,7 @@ tags=$(echo "$1" | sed -e 's/ /%20/g')
 count=$(wget -q -O - "http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=0&tags=$tags" | xmlstarlet sel -t -v /posts/@count[1])
 echo "Files in query \"$1\": $count"
 while [ "$count" -ge "0" ] ; do
-	wget -q -O - "http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&pid=$pagecount&tags=$tags" | xmlstarlet sel -t -m /posts/post/@file_url -v "." -n | while read url; do
-		wget -nc "$url"
-	done
+	wget -q -O - "http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&pid=$pagecount&tags=$tags" | xmlstarlet sel -t -m /posts/post/@file_url -v "." -n | parallel --eta -j 20 wget -nc "{}"
 	let count=count-100
 	let pagecount++
 done
