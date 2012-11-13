3271683649d32fa71b50cc3378fb4d2f971a4f1e
Sergey Alirzaev <zl29ah@gmail.com>
2012-11-13 07:16:30 +0400

    - mplayer-lastfm: added to get-aliens

--- get-aliens.sh
+++ get-aliens.sh
@@ -8,3 +8,14 @@ cd omploader.org
 get http://omploader.org/ompload ompload
 chmod +x ompload
 cd ..
+
+#emerge youtube-dl
+
+#emerge get_flash_videos
+
+#emerge youtube-viewer
+
+cd last.fm
+get "http://www.update.uu.se/~zrajm/programs/mplayer-lastfm/`http://www.update.uu.se/~zrajm/programs/mplayer-lastfm/ | sed -ne 's#.*\(mplayer-lastfm-[^<]*\)<.*#\1#p' | tail -n1`" mplayer-lastfm
+chmod +x mplayer-lastfm
+cd ..
