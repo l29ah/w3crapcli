26ae4ad670b7fc889b5038c062816e88685158d9
Sergey Alirzaev <zl29ah@gmail.com>
2013-02-10 17:54:05 +0400

    + dump.bitcheese.net: official script

--- get-aliens.sh
+++ get-aliens.sh
@@ -19,3 +19,8 @@ cd last.fm
 get "http://www.update.uu.se/~zrajm/programs/mplayer-lastfm/`http://www.update.uu.se/~zrajm/programs/mplayer-lastfm/ | sed -ne 's#.*\(mplayer-lastfm-[^<]*\)<.*#\1#p' | tail -n1`" mplayer-lastfm
 chmod +x mplayer-lastfm
 cd ..
+
+cd dump.bitcheese.net
+get 'https://gist.github.com/Voker57/221804/raw/dump.sh' dump.sh
+chmod +x dump.sh
+cd ..
