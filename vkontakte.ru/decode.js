420e9074c7aa9de7d44e65574e78e5e0d168ccbe
Sergey Alirzaev <zl29ah@gmail.com>
2017-12-02 20:36:10 +0300

    * vk.com: keeping up
    many thanks to @grisme

--- vkontakte.ru/decode.js
+++ vkontakte.ru/decode.js
@@ -5,7 +5,7 @@
             },
             r: function(e, t) {
                 e = e.split("");
-                for (var i, a = n + n, o = e.length; o--;) i = a.indexOf(e[o]), ~i && (e[o] = a.substr(i - t, 1));
+                for (var i, a = n + n, r = e.length; r--;) i = a.indexOf(e[r]), ~i && (e[r] = a.substr(i - t, 1));
                 return e.join("")
             },
             s: function(e, t) {
@@ -18,6 +18,9 @@
                 }
                 return e
             },
+            i: function(e, t) {
+                return i.s(e, t ^ vk.id)
+            },
             x: function(e, t) {
                 var n = [];
                 return t = t.charCodeAt(0), each(e.split(""), function(e, i) {
@@ -26,25 +29,25 @@
             }
         };
 
-    function o(e) {
+    function r(e) {
         if (~e.indexOf("audio_api_unavailable")) {
             var t = e.split("?extra=")[1].split("#"),
-                n = "" === t[1] ? "" : r(t[1]);
-            if (t = r(t[0]), "string" != typeof n || !t) return e;
+                n = "" === t[1] ? "" : o(t[1]);
+            if (t = o(t[0]), "string" != typeof n || !t) return e;
             n = n ? n.split(String.fromCharCode(9)) : [];
-            for (var o, s, l = n.length; l--;) {
-                if (s = n[l].split(String.fromCharCode(11)), o = s.splice(0, 1, t)[0], !i[o]) return e;
-                t = i[o].apply(null, s)
+            for (var r, s, l = n.length; l--;) {
+                if (s = n[l].split(String.fromCharCode(11)), r = s.splice(0, 1, t)[0], !i[r]);;
+                t = i[r].apply(null, s)
             }
             if (t && "http" === t.substr(0, 4)) return t
         }
         return e
     }
 
-    function r(e) {
+    function o(e) {
         if (!e || e.length % 4 == 1) return !1;
-        for (var t, i, a = 0, o = 0, r = ""; i = e.charAt(o++);) i = n.indexOf(i), ~i && (t = a % 4 ? 64 * t + i : i, a++ % 4) && (r += String.fromCharCode(255 & t >> (-2 * a & 6)));
-        return r
+        for (var t, i, a = 0, r = 0, o = ""; i = e.charAt(r++);) i = n.indexOf(i), ~i && (t = a % 4 ? 64 * t + i : i, a++ % 4) && (o += String.fromCharCode(255 & t >> (-2 * a & 6)));
+        return o
     }
 
     function s(e, t) {
@@ -52,19 +55,20 @@
             i = [];
         if (n) {
             var a = n;
-            for (t = Math.abs(t); a--;) i[a] = (t += t * (a + n) / t) % n | 0
+            for (t = Math.abs(t); a--;) t = (n * (a + 1) ^ t + a) % n, i[a] = t
         }
         return i
     }
 
-    function unwrapFixAndWrap(str) {
-        var indexOfFirstSpace = /\s/.exec(str).index;
-        var url = str.slice(0, indexOfFirstSpace);
-        return o(url) + str.slice(indexOfFirstSpace);
-    }
+function unwrapFixAndWrap(str) {
+    var indexOfFirstSpace = /\s/.exec(str).index;
+    var url = str.slice(0, indexOfFirstSpace);
+    return r(url) + str.slice(indexOfFirstSpace);
+}
 
-    var str = readline();
-    while (str !== null) {
-        print(unwrapFixAndWrap(str));
-        str = readline();
-    }
+vk = { id: readline() }
+var str = readline();
+while (str !== null) {
+    print(unwrapFixAndWrap(str));
+    str = readline();
+}
